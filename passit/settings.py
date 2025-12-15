dfe957cdb1355734b097e7d432b1b3152e6c3083
EmcioN <villly1990@gmail.com>
2025-12-15 02:54:31 +0000

    Configure templates, static, media and login

--- passit/settings.py
+++ passit/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'passit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +115,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+LOGIN_URL = "accounts:login"
+LOGIN_REDIRECT_URL = "handover:post_list"
+LOGOUT_REDIRECT_URL = "accounts:login"
