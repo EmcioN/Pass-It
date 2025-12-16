1af1fad536533bfbe221b4b8d946d6dcb3936ba1
EmcioN <villly1990@gmail.com>
2025-12-16 02:30:11 +0000

    Add login template, update urls for accounts

--- handover/views.py
+++ handover/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+@login_required
 def post_list(request):
     return render(request, "handover/post_list.html")
