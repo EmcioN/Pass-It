90210e2d5ce6f252d432138cdac976a37e774970
EmcioN <villly1990@gmail.com>
2025-12-16 06:08:14 +0000

    Update handover views to fetch posts

--- handover/views.py
+++ handover/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Post
 
 @login_required
 def post_list(request):
-    return render(request, "handover/post_list.html")
+    posts = Post.objects.all()
+    return render(request, "handover/post_list.html", {"posts": posts})
