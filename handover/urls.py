58f9fb0a6cb55e4003675698731b3ec74ce53c57
EmcioN <villly1990@gmail.com>
2025-12-24 05:59:24 +0000

    Add path for delete post

--- handover/urls.py
+++ handover/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("post/new/", views.post_create, name="post_create"),
     path("post/<int:pk>/", views.post_detail, name="post_detail"),
     path("post/<int:pk>/edit/", views.post_edit, name="post_edit"),
+    path("post/<int:pk>/delete/", views.post_delete, name="post_delete"),
 ]
