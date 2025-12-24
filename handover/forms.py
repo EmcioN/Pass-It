5a8104d10391233dec8a679476f42f4efca9effa
EmcioN <villly1990@gmail.com>
2025-12-24 04:40:56 +0000

    Add PostForm form for posts

--- handover/forms.py
+++ handover/forms.py
@@ -1,6 +1,14 @@
 from django import forms
-from .models import Comment
+from .models import Comment, Post
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "body", "team", "shift", "date_for", "status", "image"]
+        widgets = {
+            "date_for": forms.DateInput(attrs={"type": "date"}),
+        }
+        
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
