e8f6ab9a7fef305598b58250eb16eae449e5fc0c
EmcioN <villly1990@gmail.com>
2025-12-24 07:51:14 +0000

    Create post and comment model in admin

--- handover/admin.py
+++ handover/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "team", "shift", "status", "date_for", "author", "created_at")
+    list_filter = ("team", "shift", "status", "date_for")
+    search_fields = ("title", "body", "team", "author__username")
+    ordering = ("-date_for", "-created_at")
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("post", "author", "created_at")
+    search_fields = ("body", "author__username", "post__title")
