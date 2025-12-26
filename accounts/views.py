bf209d4b4eed33b363a6523e99cd337e2423449d
EmcioN <villly1990@gmail.com>
2025-12-26 03:50:14 +0000

    Create profile view and edit view

--- accounts/views.py
+++ accounts/views.py
@@ -18,3 +18,28 @@ def register(request):
 @login_required
 def profile(request):
     return render(request,"accounts/profile.html")
+
+@login_required
+def profile(request):
+    return render(request, "accounts/profile.html", {"profile": request.user.profile})
+
+
+@login_required
+def profile_edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(request.POST, instance=request.user)
+        profile_form = ProfileEditForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Profile updated.")
+            return redirect("accounts:profile")
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(request, "accounts/profile_edit.html", {
+        "user_form": user_form,
+        "profile_form": profile_form,
+    })
