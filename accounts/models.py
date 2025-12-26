acd8321b93eaad84d6f0ba8091dfa4539cffaae1
EmcioN <villly1990@gmail.com>
2025-12-26 02:58:04 +0000

    Add models for account profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="profiles/", blank=True, null=True)
+    bio = models.TextField(blank=True)
+    phone = models.CharField(max_length=30, blank=True)
+    website = models.URLField(blank=True)
+    linkedin = models.URLField(blank=True)
+    instagram = models.URLField(blank=True)
+    facebook = models.URLField(blank=True)
+
+    def __str__(self):
+        return f"Profile: {self.user.username}"
