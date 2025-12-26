7a95a3237e65663472671fa0a0f20153aac1ac9e
EmcioN <villly1990@gmail.com>
2025-12-26 06:49:08 +0000

    Fix patch in passit urls, fix accounts:logout url in navbar

--- passit/urls.py
+++ passit/urls.py
@@ -20,8 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path('admin/', admin.site.urls),    
     path("accounts/", include(("accounts.urls", "accounts"), namespace="accounts")),
     path("", include(("handover.urls", "handover"), namespace="handover")),
 ]
