e0f0172703fc4a1cde270b4ad74812aa88eabc76
EmcioN <villly1990@gmail.com>
2025-12-26 06:51:00 +0000

    Fix missing import in handover views

--- handover/views.py
+++ handover/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post
+from .models import Post, Comment
 from django.contrib import messages
 from .forms import CommentForm, PostForm
 from django.db.models import Q
