0fb6a4cd0b32b718de19eed0bd6f3c99352ff553
EmcioN <villly1990@gmail.com>
2025-12-26 03:04:12 +0000

    add profile edit form

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,3 +8,14 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "email")
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ("image", "bio", "phone", "website", "linkedin", "instagram", "facebook")
