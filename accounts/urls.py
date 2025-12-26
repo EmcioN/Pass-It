147c4c8f79e6d376e79ff62eadd750f8a2b2fa51
EmcioN <villly1990@gmail.com>
2025-12-26 04:28:37 +0000

    Update profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
+    path("profile/edit/", views.profile_edit, name="profile_edit"),
 ]
