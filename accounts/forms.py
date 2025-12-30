e5e49714adbf4db2f271f75bffb0167f7a8ab853
EmcioN <villly1990@gmail.com>
2025-12-30 05:16:49 +0000

    Fix invalid structure in registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,6 +9,11 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"    
 
 class UserEditForm(forms.ModelForm):
     class Meta:
